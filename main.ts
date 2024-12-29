771e0171af9e21e12684c8daa19c9e76f3be75ee
Francisco Bermello <juniorcede2@gmail.com>
2024-12-29 15:11:19 -0500

    enable cors

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'https://task-manager-frontend-nine-beryl.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
